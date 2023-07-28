4d763e914c3e2d7fd0bb1f866e2a501fbdfe6b0c
fadlizaki <coysiip25@gmail.com>
2023-07-28 15:25:02 +0700

    portofolio

--- src/HellooComponent.vue
+++ src/HellooComponent.vue
@@ -1,20 +1,66 @@
 <template>
-    <center><b class="p-2"
-            style="font-size: 30px; text-align: center; color: black; font-family: 'sans-serif', Calibri, 'Trebuchet MS', sans-serif;">Galeri</b>
-    </center>
-    <center> <div style="width: 10%; background: grey; height: 2px;"></div></center>
-    <br>
-        
-    <div class="grid grid-cols-3">
-        <div class="pl-20">
-            <img src="./a.png" alt="">
-        </div>
-        <div class="pl-20">
-            <img src="./b.png" alt="">
-        </div>
-        <div class="pl-20">
-            <img src="./c.png" alt="">
-        </div>
+  <div class="container mx-auto px-4">
 
+    <!-- <a href="Contoh%20Text%20Membuat%20Tombol%20Share%20Artikel%20Ke%20Whatsapp" class="share_this"><img src="share_this.png" style="height:66px;width:auto;"></a> -->
+
+    <div class="grid mb-8 border-0 md:mb-12 md:grid-cols-2">
+      <figure class="flex flex-col mb-3"><br>
+        <b class="p-2"
+          style="font-size: 30px; text-align: center; color: black; font-family: 'sans-serif', Calibri, 'Trebuchet MS', sans-serif;">Tentang
+          Saya</b>
+        <center>
+          <div style="width: 40%; background: grey; height: 5px;"></div>
+        </center>
+        <br>
+        <br>
+        <p style="font-size: 25px;" class=" ml-20">Nama saya Fadli abdullah zaki Tempat tanggal lahir Bandung.26-Juni-2005
+          Tempat tinggal saya Cibaduyut Permai saya seorang pelajar, saya sekolah di Smk Assalaam Bandung Jurusan
+          Rekayasa Perangkat Lunak </p>
+      </figure>
+      
+      <figure class="flex flex-col mb-3"><br>
+        <b class="p-2"
+            style="font-size: 30px; text-align: center; color: black; font-family: 'sans-serif', Calibri, 'Trebuchet MS', sans-serif;">Kemampuan</b>
+          <center>
+            <div style="width: 40%; background: grey; height: 5px;"></div>
+          </center>
+          <br><br>
+        <ul class=" max-w-md space-y-1 text-gray-500 list-inside dark:text-gray-400 ml-20">
+          <li class="flex items-center">
+            <svg class="w-3.5 h-3.5 mr-2 text-green-500 dark:text-green-400 flex-shrink-0" aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+              <path
+                d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+            </svg>
+            JAVASCRIPT
+          </li>
+          <li class="flex items-center">
+            <svg class="w-3.5 h-3.5 mr-2 text-green-500 dark:text-green-400 flex-shrink-0" aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+              <path
+                d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+            </svg>
+            EXPRESS.JS
+          </li>
+          <li class="flex items-center">
+            <svg class="w-3.5 h-3.5 mr-2 text-green-500 dark:text-green-400 flex-shrink-0" aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+              <path
+                d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+            </svg>
+            PHP
+          </li>
+          <li class="flex items-center">
+            <svg class="w-3.5 h-3.5 mr-2 text-green-500 dark:text-green-400 flex-shrink-0" aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+              <path
+                d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+            </svg>
+            NODE.JS
+          </li>
+        
+        </ul>
+      </figure>
     </div>
+  </div>
 </template>
